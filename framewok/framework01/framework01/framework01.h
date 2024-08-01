86f6b7516895c38f293be9278b69bbd1b37a8287
abhisheksameer <75115748+abhisheksameer@users.noreply.github.com>
2024-08-01 21:31:51 +0530

    Add files via upload

---
+++ framewok/framework01/framework01/framework01.h
@@ -0,0 +1,18 @@
+//
+//  framework01.h
+//  framework01
+//
+//  Created by abhishek sameer on 15/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for framework01.
+FOUNDATION_EXPORT double framework01VersionNumber;
+
+//! Project version string for framework01.
+FOUNDATION_EXPORT const unsigned char framework01VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <framework01/PublicHeader.h>
+
+
